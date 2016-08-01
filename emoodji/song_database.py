dcb6a3f624dd03f2945b4856da04b3a2cac43385
Demo User <demouser@MacBook-Pro.local>
2016-08-01 12:56:20 -0400

    song class incorporated

--- emoodji/song_database.py
+++ emoodji/song_database.py
@@ -1,4 +1,7 @@
-import ndb.Model
+from google.appengine.ext import ndb
 
 class Song(ndb.Model):
-    
+    title = ndb.StringProperty(required = True)
+    genre = ndb.StringProperty(required = True)
+    mood = ndb.StringProperty(required = True)
+    link = ndb.StringProperty(required = True)
