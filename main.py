36f13baf928433cb8ae786ed24c8059573356dee
Demo User <demouser@MacBook-Pro.local>
2016-08-02 11:29:45 -0400

    defining the  User model

--- main.py
+++ main.py
@@ -7,18 +7,24 @@ import webapp2
 jinja_environment = jinja2.Environment(loader=
     jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
+class User(ndb.Model):
+    name = ndb.StringProperty(required = True)
+    username = ndb.StringProperty(required = True)
+    email = ndb.StringProperty(required = True)
+    password= ndb.StringProperty(required = True)
+    genres = ndb.StringProperty(required = True)
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        template = jinja_environment.get_template('createaccount.html')
-        self.response.write(template.render())
+        user = users.get_current_user()
+        template = jinja_environment.get_template('templates/createaccount.html')
 
         if user:
             greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
                         (user.nickname(), users.create_logout_url('/')))
             self.response.out.write('%s' % greeting)
+            if
 
-            template = jinja_environment.get_template('templates/input_order.html')
-            self.response.write(template.render())
         else:
             self.response.write('<a href="%s">Sign in or register</a>.' %
                         users.create_login_url('/'))
@@ -26,14 +32,22 @@ class MainHandler(webapp2.RequestHandler):
     def post(self):
         user = users.get_current_user()
         if user:
+            #if user is logged in:
             greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
                         (user.nickname(), users.create_logout_url('/')))
+            existing_user = User.get_by_id(user.user_id())
+            if existing_user:
+                # if user is already a user, welcome 'em
+                self.response.write("Welcome %s") %(existing_user.name)
+            else:
+                #direct new user to create-account page
+                self.response.write("Welcome to our site, %s!  Please sign up! <br>")
+                self.response.write(template.render())
         else:
+            #if user not logged in:
             greeting = ('<a href="%s">Sign in or register</a>.' %
                         users.create_login_url('/'))
 
-        template = jinja_environment.get_template('createaccount.html')
-        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
