6a80b0d8c4e0c48479840f071c8866ccc4c882ae
charlinds <cal285@georgetown.edu>
2016-08-02 10:30:01 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-
+import jinja2
+import os
 import webapp2
+
+jinja_environment = jinja2.Environment(loader=
+    jinja2.FileSystemLoader(os.path.dirname(__file__)))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        template = jinja_environment.get_template('createaccount.html')
+        self.response.write(template.render())
+
+    def post(self):
+        template = jinja_environment.get_template('createaccount.html')
+        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
