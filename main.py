c0dc7555a137a8055cc00d321e2f34865b0344de
Demo User <demouser@MacBook-Pro.local>
2016-08-03 10:54:07 -0400

    models sepearte file

--- main.py
+++ main.py
@@ -15,18 +15,6 @@ class EmojiHandler(webapp2.RequestHandler):
         self.response.write(template.render())
 
 
-
-class EmoodjiUser(ndb.Model):
-    first_name = ndb.StringProperty(required = True)
-    last_name = ndb.StringProperty(required = True)
-    username = ndb.StringProperty(required = True)
-    #DON'T need email because it's already stored in google
-        #email = ndb.StringProperty(required = True)
-    # NEVER STORE PASSWORD because you can get HACKED
-        #password = ndb.StringProperty(required = True)
-    genres = ndb.StringProperty(repeated= True)
-    #repeated used for arrays
-
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         user = users.get_current_user()
